9d3aebc503e5898faf06cacaa1629a9c250474e9
Golap-mostofa <mostofa6410@gmail.com>
2022-04-18 20:56:03 +0600

    update about page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,12 +2,16 @@ import React from 'react';
 import profile from '../../img/profile.png'
 const About = () => {
     return (
-        <div>
+        <div className='container mt-5 d-flex '>
             <div>
-                <img src={profile} alt="" />
+                <img className='rounded m-4 w-90 ' src={profile} alt="" />
             </div>
-            <div>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam enim labore cum est commodi mollitia explicabo sunt vero nam unde non sequi quidem repudiandae id corporis, dignissimos distinctio tempore quisquam.
+            <div className='m-4 border p-3'>
+                <h3 className='bg-secondary p-2 text-white'>Name: Golap Mostofa</h3>
+                <p>
+                   Hello Friend i am Golap mostofa my country in bangladish i am font-end devloper my scile html css javascript javascript fremwork react and css fremwork bootstrap react bootstrap and telwiend css this project used react fermwork or libary react router firebase firebase hooks and  parfect idea my knologe simpole base and athear website soo i need job
+                </p>
+                <button className='btn btn-dark text-white'>contact as </button>
             </div>
         </div>
     );
